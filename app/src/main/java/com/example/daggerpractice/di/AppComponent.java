b73b0f52663ca975487d335fc39343d9db682cc8
Marcos Souza <marcos.souza@justa.com.vc>
2019-10-19 12:16:14 -0300

    first commit

---
+++ app/src/main/java/com/example/daggerpractice/di/AppComponent.java
@@ -0,0 +1,35 @@
+package com.example.daggerpractice.di;
+
+
+/*
+* Vai persistir em toda vida útil do aplicatico
+* */
+
+import android.app.Application;
+
+import com.example.daggerpractice.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+
+    }
+
+}
